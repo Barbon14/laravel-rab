1f511fbb5cd94464f7bcacba70d0c536bf1a0d78
BarbonFisso <barbieri.mat@hotmail.it>
2022-03-18 16:56:00 +0100

    Add: Routes

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,48 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// admin login/logout
+Route::name('admin.')
+        ->prefix('admin')
+        ->group(function () {
+            Route::post('/login', 'Auth\LoginController@login')->name('login');
+            Route::get('/logout', 'Auth\LoginController@logout')->name('logout');
 
-Auth::routes();
+            // to admin panel
+            Route::get('/panel', 'AdminController@dashboard')->name('panel');
+        });
 
-Route::get('/home', 'HomeController@index')->name('home');
+// guest
+Route::get('/', 'GuestController@home')->name('home');
+
+// articles crud
+Route::name('article.')
+        ->prefix('article')
+        ->group(function () {
+            Route::get('/show/{id}', 'ArticleController@show')->name('show');
+
+            Route::middleware('auth')
+                    ->group(function () {
+                        Route::get('/create', 'ArticleController@create')->name('create');
+                        Route::post('/store', 'ArticleController@store')->name('store');
+                        Route::get('/edit/{id}', 'ArticleController@edit')->name('edit');
+                        Route::post('/update/{id}', 'ArticleController@update')->name('update');
+                        Route::get('/delete/{id}', 'ArticleController@delete')->name('delete');
+                    });
+        });
+
+// places crud
+Route::name('place.')
+        ->prefix('place')
+        ->group(function () {
+            Route::get('/show/{id}', 'PlaceController@show')->name('show');
+
+            Route::middleware('auth')
+                    ->group(function () {
+                        Route::get('/create', 'PlaceController@create')->name('create');
+                        Route::post('/store', 'PlaceController@store')->name('store');
+                        Route::get('/edit/{id}', 'PlaceController@edit')->name('edit');
+                        Route::post('/update/{id}', 'PlaceController@update')->name('update');
+                        Route::get('/delete/{id}', 'PlaceController@delete')->name('delete');
+                    });
+        });
