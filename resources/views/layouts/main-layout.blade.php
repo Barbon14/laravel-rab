79e450270123814b3c2063cf51885c36355ad26e
BarbonFisso <barbieri.mat@hotmail.it>
2022-03-18 17:06:33 +0100

    Add: pages, components and layout

---
+++ resources/views/layouts/main-layout.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <script src="{{ asset('/js/app.js') }}"></script>
+    <link rel="stylesheet" href="{{ asset('/css/app.css') }}">
+
+    <title>
+        @yield('title', 'titolo')
+    </title>
+</head>
+<body>
+    <div id="app">
+
+        @include('components.header')
+
+        <main>
+
+            @yield('content')
+
+        </main>
+
+        @include('components.footer')
+    </div>
+    
+</body>
+</html>
---
+++ resources/views/pages/home.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.main-layout')
+
+@section('title')
+    Home | Rotelle Alla Bolognese
+@endsection
+
+@section('content')
+    content
+@endsection
