fe49fc79e85eaaf033a12e1e9ac3d1093831dde6
BarbonFisso <barbieri.mat@hotmail.it>
2022-03-21 12:37:00 +0100

    Add: show, create and edit article and place

--- app/Http/Controllers/PlaceController.php
+++ app/Http/Controllers/PlaceController.php
@@ -2,9 +2,30 @@
 
 namespace App\Http\Controllers;
 
+use App\Place;
 use Illuminate\Http\Request;
 
 class PlaceController extends Controller
 {
-    //
+    public function show($id)
+    {
+
+        $place = Place::findOrFail($id);
+
+        return view('pages.place.show', compact('place'));
+    }
+
+    public function create()
+    {
+
+        return view('pages.place.create');
+    }
+
+    public function edit($id)
+    {
+
+        $place = Place::findOrFail($id);
+
+        return view('pages.place.edit', compact('place'));
+    }
 }
