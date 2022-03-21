1ace199102b3ad6438faa2555d038d9619c91d83
BarbonFisso <barbieri.mat@hotmail.it>
2022-03-21 17:14:07 +0100

    Add: store function

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Article;
+use App\ArticlePhoto;
 use Illuminate\Http\Request;
 
 class ArticleController extends Controller
@@ -17,6 +18,37 @@ class ArticleController extends Controller
     public function create() {
 
         return view('pages.article.create');
+    }
+
+    public function store(Request $request) {
+
+        $data = $request->validate([
+
+            'title' => 'string',
+            'content' => 'string',
+            'address' => 'string',                      
+        ]);
+
+        $article = Article::make($data);
+
+        $article -> save();
+
+        // immagini 
+        $image = $request-> file('images');
+
+        $imageName = 'article' . $article -> id . '_01' . '.' . $image->getClientOriginalExtension();
+
+        $image->storeAs('/assets/', $imageName, 'public');
+
+        $imageData['photo'] = $imageName;
+
+        $photo = ArticlePhoto::make($imageData);
+
+        $photo -> article() -> associate($article->id);
+
+        $photo -> save();
+
+        return redirect()->route('article.show', $article->id);
     }
 
     public function edit($id) {
